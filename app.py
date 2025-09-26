012c5b122b1b5a669afd6ab4668a6007ec61d633
apurvaTeacher <agaygole@gmail.com>
2025-09-26 11:05:47 +0530

    update

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 def add(a, b):
     return a + b
 
+def multiply(a, b):
+    return a * b
+
 if __name__ == "__main__":
     print("Sum of 2 + 3 is:", add(2, 3))
+    print("Multiply 2 * 3 is:", multiply(2, 3))
--- test_app.py
+++ test_app.py
@@ -2,3 +2,6 @@ import app
 
 def test_add():
     assert app.add(2, 3) == 5
+
+def test_multiply():
+    assert app.multiply(2, 3) == 6
